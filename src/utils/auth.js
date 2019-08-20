8dd34804c9e76557d815a4bf3e064a3a813a5219
朱志强 <zhiqiang_zhu@bjdv.com>
2019-08-20 17:58:55 +0800

    加密解密

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,32 +1,36 @@
 import Cookies from 'js-cookie'
 import CryptoJS from 'crypto-js/crypto-js'
 
-const key = CryptoJS.enc.Utf8.parse("dadadad3￥%#*（）——+……￥#Fsaa张三");  //十六位十六进制数作为密钥
-const iv = CryptoJS.enc.Utf8.parse('+—24115—）2（31*&gaw……%￥#da');   //十六位十六进制数作为密钥偏移量
-    
-//解密方法
-export function Decrypt(word) {
-        let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
-        let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-        let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
-        let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
-        return decryptedStr.toString();
-    }
-    
+const key = "qwwe%^*yhu8976)(";
+const iv = "UJHY@#$*0987&^%H";
 //加密方法
-export function Encrypt(word) {
-        let srcs = CryptoJS.enc.Utf8.parse(word);
-        let encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
-        return encrypted.ciphertext.toString().toUpperCase();
-    }
+function encrypt(text) {
+    return CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(iv),
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    })
+}
+//解密方法
+function decrypt(text) {
+    let result = CryptoJS.AES.decrypt(text, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(iv),
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    })
+    return result.toString(CryptoJS.enc.Utf8)
+}
+
+
+
 
 const TokenKey = 'X-Auth-Token'
 const username = 'username'
 const passWord = 'passWord'
 const Catalog = 'Catalog'
-const TokenTime='TokenTime'
-const DomainName='DomainName'
-const DomainId='DomainId'
+const TokenTime = 'TokenTime'
+const DomainName = 'DomainName'
+const DomainId = 'DomainId'
 // export function setCookie(cname, cvalue, exdays) {//以分钟为基数
 //         var d = new Date();
 //         d.setTime(d.getTime() + (exdays * 60 * 1000));
@@ -50,95 +54,92 @@ const DomainId='DomainId'
 //     this.setCookie("username", "", -1);
 // } 
 export function getToken() {
-  return Cookies.get(TokenKey)
+    return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+    return Cookies.set(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+    return Cookies.remove(TokenKey)
 }
 export function getName() {
-  return Cookies.get(username)
+    return Cookies.get(username)
 }
 
 export function setName(name) {
-  return Cookies.set(username, name)
+    return Cookies.set(username, name)
 }
 
 export function removeName() {
-  return Cookies.remove(username)
+    return Cookies.remove(username)
 }
 export function getPassWord() {
-    
-    // this.Decrypt(word)
-  return Cookies.get(passWord)
+    if (Cookies.get(passWord)) {
+        return decrypt(Cookies.get(passWord))
+    } else {
+        return Cookies.get(passWord)
+    }
 }
 
 export function setPassWord(passWords) {
-    // cookie加密
-    // passWords.split("").reverse().join("")
-    
-  return Cookies.set(passWord, passWords)
-// alert(this.Encrypt(passWords))
-// return Cookies.set(passWord, this.Encrypt(passWords))
+    return Cookies.set(passWord, encrypt(passWords))
 }
 
 export function removePassWord() {
-  return Cookies.remove(passWord)
+    return Cookies.remove(passWord)
 }
 export function getCatalogs() {
-  return Cookies.get(Catalog)
+    return Cookies.get(Catalog)
 }
 
 export function setCatalogs(Catalogs) {
-  return Cookies.set(Catalog, Catalogs)
+    return Cookies.set(Catalog, Catalogs)
 }
 
 export function removeCatalogs() {
-  return Cookies.remove(Catalog)
+    return Cookies.remove(Catalog)
 }
 export function getTokenTime() {
-  return Cookies.get(TokenTime)
+    return Cookies.get(TokenTime)
 }
 
 export function setTokenTime(TokenTimes) {
-  return Cookies.set(TokenTime, TokenTimes)
+    return Cookies.set(TokenTime, TokenTimes)
 }
 
 export function removeTokenTime() {
-  return Cookies.remove(TokenTime)
+    return Cookies.remove(TokenTime)
 }
 export function getDomainId() {
-  return Cookies.get(DomainId)
+    return Cookies.get(DomainId)
 }
 
 export function setDomainId(DomainIds) {
-  return Cookies.set(DomainId, DomainIds)
+    return Cookies.set(DomainId, DomainIds)
 }
 
 export function removeDomainId() {
-  return Cookies.remove(DomainId)
+    return Cookies.remove(DomainId)
 }
 export function getDomainName() {
-  return Cookies.get(DomainName)
+    return Cookies.get(DomainName)
 }
 
 export function setDomainName(DomainNames) {
-  return Cookies.set(DomainName, DomainNames)
+    return Cookies.set(DomainName, DomainNames)
 }
 
 export function removeDomainName() {
-  return Cookies.remove(DomainName)
+    return Cookies.remove(DomainName)
 }
-export function removeAll(){
-  removeDomainName()
-  removeDomainId()
-  removeTokenTime()
-  removeCatalogs()
-  removePassWord()
-  removeName()
-  removeToken()
+export function removeAll() {
+    removeDomainName()
+    removeDomainId()
+    removeTokenTime()
+    removeCatalogs()
+    removePassWord()
+    removeName()
+    removeToken()
 }
